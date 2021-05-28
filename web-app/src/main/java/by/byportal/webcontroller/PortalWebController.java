800fd697b52b998895822d2a2321587743e274e8
orn974@gmail.com <Grigorik>
2021-05-28 15:39:26 +0300

    28 05 21

--- web-app/src/main/java/by/byportal/webcontroller/PortalWebController.java
+++ web-app/src/main/java/by/byportal/webcontroller/PortalWebController.java
@@ -18,9 +18,9 @@ public class PortalWebController {
     @GetMapping("/web/get")
     public String employeeGet (Model model){
         restTemplate=new RestTemplate();
-        ResponseEntity<Employee[]> person = restTemplate.getForEntity(ROOT_URL+"employees", Employee[].class);
+        ResponseEntity<Employee[]> person = restTemplate.getForEntity(ROOT_URL+"employeesall", Employee[].class);
         Employee[] pageList = person.getBody();
-        model.addAttribute("pageListWeb", pageList);
+        model.addAttribute("pagelistweb", pageList);
         return "HomePage";
     }
     @GetMapping("/web/post")
@@ -31,7 +31,7 @@ public class PortalWebController {
     @PostMapping("/web/post")
     public String addEmployee(Employee postEmploee) throws Exception {
         restTemplate = new RestTemplate();
-        restTemplate.postForObject(ROOT_URL + "/emploees",postEmploee,Employee.class);
+        restTemplate.postForObject(ROOT_URL + "emploees",postEmploee,Employee.class);
         return "redirect:/web/get";
     }
    /* @GetMapping("/web/put/{id}")
@@ -41,13 +41,13 @@ public class PortalWebController {
     @PutMapping("/web/put")
     public String putEmploeeOne (@PathVariable(value = "id") long id, Employee putEmploee) throws Exception {
         restTemplate = new RestTemplate();
-        restTemplate.put(ROOT_URL + "/emploees/{id}", putEmploee, Employee.class);
+        restTemplate.put(ROOT_URL + "emploees/{id}", putEmploee, Employee.class);
         return "redirect:/web/get";
     }
     @PostMapping("/web/del/{id}")
     public String delEmploeeOne (@PathVariable(value = "id") long id, Model model){
         restTemplate = new RestTemplate();
-        restTemplate.delete(ROOT_URL + "/emploees/{id}", Employee.class);
+        restTemplate.delete(ROOT_URL + "emploees/{id}", Employee.class);
         return "redirect:/web/get";
     }
 }
