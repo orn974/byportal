66d072abe50967609b1abd0fa07d136a951e1c1b
orn974@gmail.com <Grigorik>
2021-12-18 11:25:34 +0300

    Save and Read Excel

--- sshwa/src/main/java/com/elennaro/sshwa/config/SecurityInit.java
+++
@@ -1,8 +0,0 @@
-package com.elennaro.sshwa.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-
-	
-}
